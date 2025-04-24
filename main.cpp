c0229d6eb1f7f7b7221f03b4d48d7e7d35188038
Tikhonov Alexander <tadphp@yandex.ru>
2025-04-24 18:17:26 +0300

    add json base parce, HTTP ver for ubuntu

--- main.cpp
+++ main.cpp
@@ -1,10 +1,45 @@
-#include <userver/components/run.hpp>
-#include "hello_handler.h"
-#include "src/handlers/hello_handler.h"
+#include <fstream>
+#include <iostream>
+#include <string_view>
+#include <userver/utest/using_namespace_userver.hpp>
+#include <userver/server/handlers/http_handler_base.hpp>
+#include <userver/components/minimal_server_component_list.hpp>
+#include <userver/utils/daemon_run.hpp>
+#include "PCF/PCF_json_bridge.h"
 
-int main(int argc, char* argv[]) {
-    auto component_list = userver::components::ComponentList()
-        .Append<samples::hello::HelloHandler>();
+class AboutHandler final : public server::handlers::HttpHandlerBase {
+public:
+    static constexpr std::string_view kName = "handler-about";
+    using HttpHandlerBase::HttpHandlerBase;
 
-    return userver::components::Run(argc, argv, component_list);
+    std::string HandleRequestThrow(const server::http::HttpRequest&,
+                                   server::request::RequestContext&) const override {
+        return "Hello World! I am a new BSF node!";
+    }
+};
+
+int main() {
+    std::string file = "../data.json";
+    std::ifstream in(file);
+    if (!in.is_open()) {
+        std::cout << "Не удалось открыть файл!" << std::endl;
+        return 1;
+    }
+
+    std::string input((std::istreambuf_iterator<char>(in)),
+                       std::istreambuf_iterator<char>());
+    in.close();
+
+    json j = json::parse(input);
+
+    PCF_dto dto = j.get<PCF_dto>();
+    std::cout << "parsing from JSON complete!" << std::endl;
+    std::cout << "Parsed SUPI: " << (dto.supi ? *dto.supi : "(none)") << std::endl;
+    std::cout << "DNN: " << dto.dnn << std::endl;
+    std::cout << "SST: " << dto.snssai.sst << std::endl;
+    std::cout << std::endl;
+    json out = dto;
+    std::cout << "parsing from raw data complete" <<std::endl;
+    std::cout << "Serialized JSON:\n" << out.dump(2) << std::endl;
+    return 0;
 }
